79ce577f26394b46fec032a048e3850377e90cc4
madrimovDev <madrimov5014@gmail.com>
2023-03-04 18:18:01 +0500

    getstatic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,16 @@
 import { Hero, Iam, Navbar, Skills } from '@/components'
-import { GetStaticProps } from 'next'
-import { DataType } from './types';
+import { GetServerSideProps, GetStaticProps } from 'next'
+import { DataType } from './types'
+import { useEffect, useState } from 'react'
+import path from 'path'
+import fs from 'fs/promises'
 
-export default function Home({ result }: { result: DataType }) {
+export default function Home({
+	result
+}: {
+	result: DataType
+	}) {
+	
 	console.log(result);
 	
 	return (
@@ -17,12 +25,19 @@ export default function Home({ result }: { result: DataType }) {
 	)
 }
 
-export const getStaticProps: GetStaticProps = async (context) => {
-	const response = await fetch('/api/hero')
-	const result = await response.json()
+export const getStaticProps: GetStaticProps = async () => {
+	
+	const dataDir = path.join(
+		process.cwd(),
+		'src/pages/api/hero/data.json'
+	)
+	const file = await fs.readFile(dataDir, {
+		encoding: 'utf-8'
+	})
+
 	return {
 		props: {
-			result: result
+			result: JSON.parse(file) as unknown as DataType
 		}
 	}
 }
