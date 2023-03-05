911772d06271553c4fe57160687f822b607c0802
madrimovDev <madrimov5014@gmail.com>
2023-03-05 20:59:42 +0500

    animation fix

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,35 +1,26 @@
 import React, { FC, PropsWithChildren, useRef } from 'react'
 import Navbar from './navbar/Navbar'
 import Footer from './footer/Footer'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
-	
-  const ref = useRef<HTMLElement>(null)
-  const padding = ref.current ? ref.current.clientHeight : 0
-  
+	const ref = useRef<HTMLElement>(null)
+	const padding = ref.current ? ref.current.clientHeight : 0
+
 	return (
 		<div
 			style={{
 				paddingTop: padding
 			}}
 			className='container px-4 md:px-0 max-w-screen-sm mx-auto'
-    >
-      <motion.div animate={{
-        rotate: 360,
-        transition: {
-          type: 'tween',
-          repeat: Infinity,
-          repeatType: 'loop',
-          duration: 24
-        }
-      }} className='fixed inset-0 opacity-30 -z-10'>
-        <div  className='w-1/2 absolute top-0 left-0 rounded-full blur-[200px] opacity-50 aspect-square bg-gradient-to-tr to-blue-500 from-violet-500'/>
-        <div  className='w-1/2 absolute bottom-0 right-0 rounded-full blur-[200px] opacity-20 aspect-square bg-gradient-to-tr to-red-500 from-violet-500'/>
-      </motion.div>
+		>
+			<div className='animate-spin-slow fixed inset-0 opacity-30 -z-10'>
+				<div className='w-1/2 absolute top-0 left-0 rounded-full blur-[200px] opacity-50 aspect-square bg-gradient-to-tr to-blue-500 from-violet-500' />
+				<div className='w-1/2 absolute bottom-0 right-0 rounded-full blur-[200px] opacity-20 aspect-square bg-gradient-to-tr to-red-500 from-violet-500' />
+			</div>
 			<Navbar ref={ref} />
-      {children}
-      <Footer />
+			{children}
+			<Footer />
 		</div>
 	)
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	darkMode: 'class',
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			animation: {
+				'spin-slow': 'spin 13s linear infinite'
+			}
+		}
+	},
+	plugins: []
 }
