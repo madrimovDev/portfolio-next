e16ff7fb5eaa57b62f75cab6c026b8e7fe5719ec
madrimovDev <madrimov5014@gmail.com>
2023-03-13 11:46:07 +0500

    g analitic

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
 	return (
@@ -10,6 +11,21 @@ export default function Document() {
 			>
 				<Main />
 				<NextScript />
+				<Script
+					async
+					src='https://www.googletagmanager.com/gtag/js?id=G-6BSPQHBC28'
+				/>
+				<Script
+					id='google-analytics'
+					strategy='afterInteractive'
+					dangerouslySetInnerHTML={{
+						__html: `  window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+					
+						gtag('config', 'G-6BSPQHBC28');`
+					}}
+				/>
 			</body>
 		</Html>
 	)
