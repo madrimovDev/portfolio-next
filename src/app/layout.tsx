5bf923bf2eb0ddb0746a6a241b89fc2b34624a13
madrimovDev <madrimov5014@gmail.com>
2024-02-19 14:46:36 +0500

    analitics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Beta from "~/components/beta/beta";
-
+import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({
 	subsets: ["latin"],
 });
@@ -23,6 +23,7 @@ export default async function RootLayout({
 			<body className={`${inter.className} h-full flex flex-col`}>
 				{children}
 				<Beta />
+				<Analytics />
 			</body>
 		</html>
 	);
