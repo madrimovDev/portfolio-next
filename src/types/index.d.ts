c473d350fc569b1946d8f1db0060853f4a72733b
madrimovDev <madrimov5014@gmail.com>
2026-06-14 13:22:41 +0500

    feat(types): add BlogPostMeta for Notion blog

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -9,3 +9,12 @@ export type PropsWithLang = {
 }
 
 export type Lang = 'en' | 'uz' | 'ru'
+
+export type BlogPostMeta = {
+	id: string;
+	slug: string;
+	title: string;
+	description: string;
+	date: string;
+	tags: string[];
+};
