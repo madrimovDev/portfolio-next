eab2cc681dc8860ee1e89d78561dffe1e56ea2bf
madrimovDev <madrimov5014@gmail.com>
2026-06-14 15:23:28 +0500

    feat(images): allow Notion S3 image hosts

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{ protocol: "https", hostname: "prod-files-secure.s3.us-west-2.amazonaws.com" },
+			{ protocol: "https", hostname: "s3.us-west-2.amazonaws.com" },
+			{ protocol: "https", hostname: "*.amazonaws.com" },
+		],
+	},
+};
 
 export default nextConfig;
-
